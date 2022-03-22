0323355b1225fdfade698eec73045f23e10e9fe6
Callum Hunter <55032230+Stargate18@users.noreply.github.com>
2022-03-22 16:57:50 +0000

    Completed SwimEntry class

---
+++ src/main/java/com/stir/cscu9t4practical1/SwimEntry.java
@@ -0,0 +1,25 @@
+package com.stir.cscu9t4practical1;
+
+public class SwimEntry extends Entry {
+	
+	private String where;
+	
+	public SwimEntry(String n, int d, int m, int y, int h, int min, int s, float dist, String w) {
+		super(n, d, m, y, h, min, s, dist);
+		this.where = w;
+	}
+
+	public String getWhere() {
+		if (this.where.equals("pool")) {
+			return "in a pool";
+		}
+		return this.where;
+	}
+	
+	public String getEntry() {
+		String result = getName() + " swam " + getDistance() + " km " + getWhere() + " in " + getHour() + ":" + getMin() + ":" + getSec()
+				+ " on " + getDay() + "/" + getMonth() + "/" + getYear() + "\n";
+		return result;
+	}
+
+}
